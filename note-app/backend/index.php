384c81872691a47b5158aa3166ec48216898426e
alexbalak21 <alex.balak@outlook.com>
2025-03-03 17:28:04 +0100

    router done

--- note-app/backend/index.php
+++ note-app/backend/index.php
@@ -1,42 +1,8 @@
 <?php
+require_once "Rooter.php";
+$rooter = new Rooter();
 
-function method($method){
-    $method = $_SERVER['REQUEST_METHOD'];
-    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    switch ($method) {
-    case 'GET':
-        # code...
-        break;
-    case 'POST':
-        # code...
-        break;
-    case 'PUT':
-        # code...
-        break;
-    case 'DELETE':
-        # code...
-        break;
-    
-    default:
-        # code...
-        break;
-}
-}
-
-function GET($uri){
-
-}
-
-function POST($uri){
-
-}
-
-function PUT($uri){
-
-}
-
-function DELETE($uri){
-
-}
+$rooter->GET("/", function () {
+    echo "Hello World!";    });
 
 ?>
