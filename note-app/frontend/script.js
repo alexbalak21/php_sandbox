c758cb0eb69f61e9e506ed10adf891ae7dd994e2
Alex Balak <alex.balak@outlook.com>
2025-05-20 15:13:47 +0200

    trying to set width right

--- note-app/frontend/script.js
+++ note-app/frontend/script.js
@@ -2,18 +2,38 @@
 const quill = new Quill("#editor", {
   modules: {
     toolbar: [
-      [{ header: [1, 2, 3, false] }],
+      [{header: [1, 2, 3, false]}],
       ["bold", "italic", "underline"],
       ["image"],
-      [{ list: "ordered" }, { list: "bullet" }], // Add list options
-      [{ align: ["", "center", "right"] }], // Add alignment options
+      [{list: "ordered"}, {list: "bullet"}], // Add list options
+      [{align: ["", "center", "right"]}], // Add alignment options
     ],
   },
   placeholder: "Note...",
   theme: "snow", // or 'bubble'
-});
+})
 
-const form = document.getElementById("editor-form");
+const form = document.getElementById("editor-form")
 form.onsubmit = () => {
-  document.getElementById("editor-content").value = document.querySelector(".ql-editor").innerHTML;
-};
+  document.getElementById("editor-content").value = document.querySelector(".ql-editor").innerHTML
+}
+
+//APPENDING ITEMS TO THE LIST
+
+const list_item = document.querySelector(".list-group-item")
+const list = document.querySelector("#list")
+
+function append_item(number) {
+  for (let i = 0; i < number; i++) {
+    const new_item = list_item.cloneNode(true)
+    new_item.classList.remove("active")
+    list.appendChild(new_item)
+  }
+}
+
+append_item(2)
+
+const aside = document.querySelector("aside")
+const main = document.querySelector("main")
+aside.style.width = "50%"
+main.style.width = "50%"
