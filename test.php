92329f9d94c216d5cd711833fbd42507ab2ae5e7
Alex Balak <alex.balak@outlook.com>
2025-07-07 11:21:26 +0200

    testing new css tips, dose not work.

--- test.php
+++ test.php
@@ -1,6 +1,24 @@
-<?php
-$v = null ?? "hello";
+<!DOCTYPE html>
+<html lang="en">
 
-printf($v);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-?>
+<body>
+    <main>
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    </main>
+
+</body>
+<style>
+    div {
+        align-content: center;
+    }
+</style>
+
+</html>
